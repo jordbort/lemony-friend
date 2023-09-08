71ad0adc3041987bdca9816c1c51a40d3693b557
jordbort <jordan.bortner@gmail.com>
2023-09-07 23:40:42 -0400

    Adding sclarf subtember goals

--- bot.js
+++ bot.js
@@ -170,6 +170,61 @@ function onMessageHandler(chatroom, tags, msg, self) {
         else { return talk(chatroom, `Greetings, ${users[username].displayName}! :)`) }
     }
 
+    // sclarf SUBtember goals
+    if (command === `!goals`
+        && args.length
+        && chatroom === sclarf) {
+        const subs = Number(args[0])
+        const goals = {
+            25: `sclarf will play 3d pinball!`,
+            50: `sclarf will make a food tirr list!`,
+            75: `sclarf will have a fight club movie night!`,
+            100: `sclarf will play hello kitty island adventure!`,
+            125: `sclarf will play a 400-in-1 games console!`,
+            150: `chat decidecs speedrun`,
+            175: `sclarf will play just dance!`,
+            200: `sclarf will do a cosplay!`,
+            225: `lemony fresh stream???`,
+            250: `sclarf will play Pokemon Soul Link!`,
+            275: `mezcal tasting when pp`,
+            300: `bianca stream`,
+            325: `bianca cosplay`,
+            350: `sclarf will do an art stream!`,
+            375: `sclarf will have a casino night!`,
+            400: `sclarf will play Pokemon Infinite fusion`,
+            425: `chat pick cosplay`,
+            450: `sclarf will game give away`,
+            475: `big collab`,
+            500: `sclarf will make a server in minecraft!`,
+            525: `sclarf will play Dream Daddy or another dating sim!`,
+            550: `LEGO STREAM`,
+            575: `cooking adjacent goal`,
+            600: `sclarf will END STREAM IMMEDIATELY`,
+            625: `sclarf will Call shannon wonderwall`,
+            650: `sclarf will tace bell irl stream?`,
+            675: `sclarf will sexc sclarf corp calendar`,
+            700: `sclarf will go see trom!`
+        }
+        if (isNaN(subs)) {
+            return talk(chatroom, subs)
+        } else if (subs in goals) {
+            return talk(channel, `At ${subs} subs, ${goals[subs]}`)
+        } else {
+            const adjectives = [
+                `nice`,
+                `nice`,
+                `friendly`,
+                `friendly`,
+                `sweet`,
+                `lovely`,
+                `warm`,
+                `special`,
+                `consensual`
+            ]
+            return talk(chatroom, `At ${subs} subs, sclarf will give @lemony_friend a ${adjectives[Math.floor(Math.random() * adjectives.length)]} hug! :)`)
+        }
+    }
+
     // !bye OR !gn OR !goodnight
     if (command === `!bye`
         || command === `!gn`
