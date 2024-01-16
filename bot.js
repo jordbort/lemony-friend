b89b186077ffaa7a1a0acf57ac063105a4b6eb19
jordbort <jordan.bortner@gmail.com>
2024-01-16 14:21:26 -0500

    Abstracting goals command

--- bot.js
+++ bot.js
@@ -123,6 +123,7 @@ const { useLemon } = require("./handleUseLemon")
 const {
     client,
     handleUncaughtException,
+    sayGoals,
     sayRebootMsg,
     sayFriends,
     sayCommands,
@@ -452,51 +453,10 @@ ${redBg}lemony_friend has died.${resetTxt}`)
     // Check for tempCmd
     if (command in tempCmds) { return talk(chatroom, tempCmds[command]) }
 
-    // sclarf SUBtember goals
+    // Handle channel-specific goals
     if (command === `!goals`
         && args.length) {
-        if (chatroom === `#sclarf`) {
-            const subs = Number(args[0])
-            const sclarfGoals = {
-                25: `sclarf will play 3d pinball!`,
-                50: `sclarf will make a food tirr list!`,
-                75: `sclarf will have a fight club movie night!`,
-                100: `sclarf will play hello kitty island adventure!`,
-                125: `sclarf will play a 400-in-1 games console!`,
-                150: `chat decidecs speedrun`,
-                175: `sclarf will play just dance!`,
-                200: `sclarf will do a cosplay!`,
-                225: `lemony fresh stream???`,
-                250: `sclarf will play Pokemon Soul Link!`,
-                275: `mezcal tasting when pp`,
-                300: `bianca stream`,
-                325: `bianca cosplay`,
-                350: `sclarf will do an art stream!`,
-                375: `sclarf will have a casino night!`,
-                400: `sclarf will play Pokemon Infinite fusion`,
-                425: `chat pick cosplay`,
-                450: `sclarf will game give away`,
-                475: `big collab`,
-                500: `sclarf will make a server in minecraft!`,
-                525: `sclarf will play Dream Daddy or another dating sim!`,
-                550: `LEGO STREAM`,
-                575: `cooking adjacent goal`,
-                600: `sclarf will END STREAM IMMEDIATELY`,
-                625: `sclarf will Call shannon wonderwall`,
-                650: `sclarf will taco bell irl stream?`,
-                675: `sclarf will sexc sclarf corp calendar`,
-                700: `sclarf will go see trom!`
-            }
-            if (!isNaN(subs) && subs in sclarfGoals) { return talk(chatroom, `At ${subs} subs, ${sclarfGoals[subs]}`) }
-        } else if (chatroom === `#domonintendo1`) {
-            const dollars = Number(args[0])
-            const domoGoals = {
-                50: `domo will do a 12-hour stream!`,
-                100: `domo will do a 12-hour stream + read a children's book in Japanese!`,
-                150: `domo will play Pokemon Fire Red in Japanese until we beat it!`
-            }
-            if (!isNaN(dollars) && dollars in domoGoals) { return talk(chatroom, `At $${dollars}, ${domoGoals[dollars]}`) }
-        }
+        return sayGoals(chatroom, args)
     }
 
     // !bye OR !gn OR !goodnight
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const BOT_USERNAME = process.env.BOT_USERNAME
 const OAUTH_TOKEN = process.env.OAUTH_TOKEN
 
 // Import global settings
-const { resetTxt, boldTxt, yellowBg, chatColors, settings } = require(`./config`)
+const { resetTxt, boldTxt, grayTxt, yellowBg, chatColors, settings } = require(`./config`)
 
 // Import data
 const { lemonyFresh, users, tempCmds } = require(`./data`)
@@ -76,6 +76,52 @@ async function handleUncaughtException(errMsg, location) {
     return lemonyFresh.channels.forEach((channel) => {
         talk(channel, `Oops, I just crashed! ${users[BOT_USERNAME]?.sclarf?.sub ? `sclarfDead` : `>(`} ${errMsg} ${location}`)
     })
+}
+
+function sayGoals(chatroom, args) {
+    if (settings.debug) { console.log(`${boldTxt}> sayGoals(chatroom: ${chatroom}, args: ${args})${resetTxt}`) }
+    if (chatroom === `#sclarf`) {
+        const subs = Number(args[0])
+        const sclarfGoals = {
+            25: `sclarf will play 3d pinball!`,
+            50: `sclarf will make a food tirr list!`,
+            75: `sclarf will have a fight club movie night!`,
+            100: `sclarf will play hello kitty island adventure!`,
+            125: `sclarf will play a 400-in-1 games console!`,
+            150: `chat decidecs speedrun`,
+            175: `sclarf will play just dance!`,
+            200: `sclarf will do a cosplay!`,
+            225: `lemony fresh stream???`,
+            250: `sclarf will play Pokemon Soul Link!`,
+            275: `mezcal tasting when pp`,
+            300: `bianca stream`,
+            325: `bianca cosplay`,
+            350: `sclarf will do an art stream!`,
+            375: `sclarf will have a casino night!`,
+            400: `sclarf will play Pokemon Infinite fusion`,
+            425: `chat pick cosplay`,
+            450: `sclarf will game give away`,
+            475: `big collab`,
+            500: `sclarf will make a server in minecraft!`,
+            525: `sclarf will play Dream Daddy or another dating sim!`,
+            550: `LEGO STREAM`,
+            575: `cooking adjacent goal`,
+            600: `sclarf will END STREAM IMMEDIATELY`,
+            625: `sclarf will Call shannon wonderwall`,
+            650: `sclarf will taco bell irl stream?`,
+            675: `sclarf will sexc sclarf corp calendar`,
+            700: `sclarf will go see trom!`
+        }
+        if (!isNaN(subs) && subs in sclarfGoals) { talk(chatroom, `At ${subs} subs, ${sclarfGoals[subs]}`) }
+    } else if (chatroom === `#domonintendo1`) {
+        const dollars = Number(args[0])
+        const domoGoals = {
+            50: `domo will do a 12-hour stream!`,
+            100: `domo will do a 12-hour stream + read a children's book in Japanese!`,
+            150: `domo will play Pokemon Fire Red in Japanese until we beat it!`
+        }
+        if (!isNaN(dollars) && dollars in domoGoals) { talk(chatroom, `At $${dollars}, ${domoGoals[dollars]}`) }
+    } else { console.log(`${grayTxt}${chatroom.substring(1)} has no goals${resetTxt}`) }
 }
 
 function sayRebootMsg(chatroom) {
@@ -274,6 +320,7 @@ function talk(chatroom, msg) {
 module.exports = {
     client,
     handleUncaughtException,
+    sayGoals,
     sayRebootMsg,
     sayFriends,
     sayCommands,
