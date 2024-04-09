bad69a75c6d089ca164dfbc8815d2d069995d8ae
jordbort <jordan.bortner@gmail.com>
2024-04-08 21:04:20 -0400

    Add channel nicknames

--- commands/getInfo.js
+++ commands/getInfo.js
@@ -1,4 +1,4 @@
-const { users } = require(`../data`)
+const { lemonyFresh, users } = require(`../data`)
 const { resetTxt, grayTxt, chatColors, settings } = require(`../config`)
 
 const { numbers, getLemonEmote, getDumbEmote, pluralize, getNeutralEmote, getToUser, getPositiveEmote, getHypeEmote } = require(`../utils`)
@@ -18,9 +18,16 @@ module.exports = {
         const userObjNickname = targetNickname || userNickname
         if (settings.debug) { console.log(`${grayTxt}> getLastMessage(chatroom: '${chatroom}', userObj: '${target ? toUser : username}, otherChannel: '${otherChannel}')${resetTxt}`) }
 
+        const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
+        const otherChannelNickname = otherChannel in lemonyFresh
+            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
+            : null
+
         otherChannel in userObj
-                ? bot.say(chatroom, `${userObjNickname} last said: "${userObj[otherChannel].lastMessage}" in ${otherChannel}'s chat!`)
-                : bot.say(chatroom, `${userObjNickname} last said: "${userObj[channel].lastMessage}" in ${channel}'s chat!`)
+            ? bot.say(chatroom, `${userObjNickname} last said: "${userObj[otherChannel].lastMessage}" in ${otherChannelNickname}'s chat!`)
+            : channel in userObj
+                ? bot.say(chatroom, `${userObjNickname} last said: "${userObj[channel].lastMessage}" in ${channelNickname}'s chat!`)
+                : bot.say(chatroom, `${userObjNickname} hasn't spoken in ${channelNickname}'s chat!`)
     },
     getMessageCount(props) {
         const { bot, chatroom, username, user, userNickname, toUser, target, targetNickname } = props
@@ -39,15 +46,16 @@ module.exports = {
         bot.say(chatroom, response)
     },
     getColor(props) {
-        const { bot, chatroom, channel, user, target } = props
+        const { bot, chatroom, channel, username, user, userNickname, toUser, target, targetNickname } = props
         const userObj = target || user
-        if (settings.debug) { console.log(`${grayTxt}> getColor(chatroom: ${chatroom}, userObj: ${userObj.displayName})${resetTxt}`) }
+        const userObjNickname = targetNickname || userNickname
+        if (settings.debug) { console.log(`${grayTxt}> getColor(chatroom: ${chatroom}, userObj: '${target ? toUser : username})${resetTxt}`) }
 
         !userObj.color
-            ? bot.say(chatroom, `I can't tell what ${userObj.displayName}'s chat color is! ${getDumbEmote(channel)}`)
+            ? bot.say(chatroom, `I can't tell what ${userObjNickname}'s chat color is! ${getDumbEmote(channel)}`)
             : userObj.color in chatColors
-                ? bot.say(chatroom, `${userObj.displayName}'s chat color is ${chatColors[userObj.color].name}!`)
-                : bot.say(chatroom, `${userObj.displayName}'s chat color is hex code ${userObj.color}`)
+                ? bot.say(chatroom, `${userObjNickname}'s chat color is ${chatColors[userObj.color].name}!`)
+                : bot.say(chatroom, `${userObjNickname}'s chat color is hex code ${userObj.color}`)
     },
     getRandomUser(arrExclude) {
         if (settings.debug) { console.log(`${grayTxt}> getRandomUser(arrExclude:`, arrExclude, `${grayTxt})${resetTxt}`) }
