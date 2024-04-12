d4d67eed0ff91a09a56fd596d0423342a5c651d4
jordbort <jordan.bortner@gmail.com>
2024-04-11 23:25:22 -0400

    Add rename functionality

--- commands/tempCmds.js
+++ commands/tempCmds.js
@@ -12,15 +12,17 @@ module.exports = {
         if (!args[1]) { return bot.say(chatroom, `Hey ${userNickname}, say "!tempcmd <command_name> <command_response>..." to add/edit a command, or say "!tempcmd delete <command_name>" to delete a command. You can also use !tempcmds to view all of them! :)${neutralEmote}`) }
 
         const negativeEmote = getNegativeEmote(channel)
-        if (args[0].toLowerCase() === `delete`) {
-            if (args[1].toLowerCase() in tempCmds) {
-                delete tempCmds[args[1].toLowerCase()]
-                bot.say(chatroom, `Command "${args[1].toLowerCase()}" has been deleted! ${positiveEmote}`)
-            } else {
-                bot.say(chatroom, `No command "${args[1].toLowerCase()}" was found! ${negativeEmote}`)
-            }
-        }
-        else if (args[0].toLowerCase() in tempCmds) {
+        if (/^delete$/i.test(args[0])) {
+            if (!(args[1].toLowerCase() in tempCmds)) { return bot.say(chatroom, `No command "${args[1].toLowerCase()}" was found! ${negativeEmote}`) }
+            delete tempCmds[args[1].toLowerCase()]
+            bot.say(chatroom, `Command "${args[1].toLowerCase()}" has been deleted! ${positiveEmote}`)
+        } else if (/^rename$/i.test(args[0])) {
+            if (!(args[1].toLowerCase() in tempCmds)) { return bot.say(chatroom, `No command "${args[1].toLowerCase()}" was found! ${negativeEmote}`) }
+            if (!args[2]) { return bot.say(chatroom, `Hey ${userNickname}, I need to know what you want me to rename ${args[1].toLowerCase()} to! :O`) }
+            tempCmds[args[2].toLowerCase()] = tempCmds[args[1].toLowerCase()]
+            delete tempCmds[args[1].toLowerCase()]
+            bot.say(chatroom, `Command "${args[1].toLowerCase()}" has been renamed to "${args[2].toLowerCase()}"! ${positiveEmote}`)
+        } else if (args[0].toLowerCase() in tempCmds) {
             tempCmds[args[0].toLowerCase()] = args.slice(1).join(` `)
             bot.say(chatroom, `Command "${args[0].toLowerCase()}" has been edited! ${positiveEmote}`)
         } else {
