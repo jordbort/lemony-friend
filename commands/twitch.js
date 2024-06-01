c9a4520b78d3f5c78021f41a5350abbbccc0f52d
jordbort <jordan.bortner@gmail.com>
2024-06-01 14:50:35 +0200

    Correct object logging

--- commands/twitch.js
+++ commands/twitch.js
@@ -128,7 +128,7 @@ async function getTwitchChannel(bot, chatroom, broadcaster_id) {
 
     const response = await fetch(endpoint, options)
     const twitchData = await response.json()
-    logMessage([`getTwitchChannel`, response.status, renderObj(twitchData, `twitchData`)])
+    logMessage([`getTwitchChannel`, response.status, `data` in twitchData ? twitchData.data.length ? renderObj(twitchData.data[0], `twitchData.data[0]`) : `twitchData.data: []` : renderObj(twitchData, `twitchData`)])
 
     const channel = chatroom.substring(1)
     if (response.status !== 200) { return bot.say(chatroom, `${twitchData?.message || `There was a problem getting the channel info!`} ${getNegativeEmote(channel)}`) }
@@ -167,7 +167,7 @@ module.exports = {
 
         const response = await fetch(endpoint, options)
         const twitchData = await response.json()
-        logMessage([`pollEnd`, response.status, renderObj(twitchData, `twitchData`)])
+        logMessage([`pollEnd`, response.status, renderObj(twitchData.data[0], `twitchData.data[0]`)])
 
         if (`error` in twitchData) {
             bot.say(chatroom, `Error ending poll! ${negativeEmote}`)
@@ -235,7 +235,7 @@ module.exports = {
 
         const response = await fetch(endpoint, options)
         const twitchData = await response.json()
-        logMessage([`pollStart`, response.status, renderObj(twitchData, `twitchData`)])
+        logMessage([`pollStart`, response.status, `data` in twitchData ? twitchData.data.length ? renderObj(twitchData.data[0], `twitchData.data[0]`) : `twitchData.data: []` : renderObj(twitchData, `twitchData`)])
 
         const positiveEmote = getPositiveEmote(channel)
         if (response.status === 200) {
@@ -250,7 +250,7 @@ module.exports = {
             options.headers.authorization = `Bearer ${lemonyFresh[channel].accessToken}`
             const finalAttempt = await fetch(endpoint, options)
             const finalAttemptData = await finalAttempt.json()
-            logMessage([`pollStart`, finalAttempt.status, renderObj(finalAttemptData, `finalAttemptData`)])
+            logMessage([`pollStart`, finalAttempt.status, `data` in finalAttemptData ? finalAttemptData.data.length ? renderObj(finalAttemptData.data[0], `finalAttemptData.data[0]`) : `finalAttemptData.data: []` : renderObj(finalAttemptData, `finalAttemptData`)])
             if (finalAttempt.status === 200) {
                 bot.say(chatroom, `Poll created, go vote! ${positiveEmote}`)
                 lemonyFresh[channel].pollId = finalAttemptData.data[0].id
