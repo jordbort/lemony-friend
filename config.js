b7c08d5fdce50409fbbbd2a5c2049c0c6fa7b1fe
jordbort <jordan.bortner@gmail.com>
2024-09-15 03:22:15 -0400

    Change default pokeball value

--- config.js
+++ config.js
@@ -73,7 +73,7 @@ const settings = {
     hangmanLemonThreshold: 2,
     chantCount: 8,
     chantEmote: `👏️`,
-    pokeballQuantity: 100,
+    pokeballQuantity: 50,
     ignoredBots: [
         BOT_USERNAME,
         `blerp`,
