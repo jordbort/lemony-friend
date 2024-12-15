3f5c4319f774bf3b80b4ce895d0764d84f79c0cb
jordbort <jordan.bortner@gmail.com>
2024-12-15 15:31:23 -0500

    Refactor logic for asking sub/mod/vip in current or other channel with nicknames

--- patterns/checkChannelInfo.js
+++ patterns/checkChannelInfo.js
@@ -4,110 +4,136 @@ const { getHypeEmote, getNegativeEmote, getDumbEmote, getToUser, pluralize, logM
 module.exports = {
     checkEmotes(props, splitMessage) {
         const { bot, chatroom, userNickname } = props
-        const capturedChannel = getToUser(splitMessage[1])
-        logMessage([`> checkEmotes(chatroom: '${chatroom}', capturedChannel: '${capturedChannel}', userNickname: '${userNickname}')`])
+        const toUser = getToUser(splitMessage[1])
+        const toUserByNickname = findUserByNickname(toUser)
+        const validatedChannelName = toUserByNickname in lemonyFresh
+            ? toUserByNickname
+            : toUser in lemonyFresh
+                ? toUser
+                : null
+        const channelNickname = users[validatedChannelName]?.nickname || users[validatedChannelName]?.displayName || validatedChannelName
+        logMessage([`> checkEmotes(chatroom: '${chatroom}', toUser: '${toUser}', validatedChannelName: '${validatedChannelName}', channelNickname: '${channelNickname}')`])
 
-        if (capturedChannel in lemonyFresh) {
-            `emotes` in lemonyFresh[capturedChannel]
-                ? bot.say(chatroom, `I know ${pluralize(lemonyFresh[capturedChannel].emotes.length, `emote`, `emotes`)} in ${capturedChannel}'s channel!`)
-                : bot.say(chatroom, `I don't know how many emotes ${capturedChannel} has!`)
+        if (validatedChannelName in lemonyFresh) {
+            `emotes` in lemonyFresh[validatedChannelName]
+                ? bot.say(chatroom, `I know ${pluralize(lemonyFresh[validatedChannelName].emotes.length, `emote`, `emotes`)} in ${channelNickname}'s channel, ${userNickname}!`)
+                : bot.say(chatroom, `I don't know how many emotes ${channelNickname} has, ${userNickname}!`)
         }
     },
     checkSelfSub(props, splitMessage) {
         const { bot, chatroom, channel, user, userNickname } = props
-        const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
+        const currentChannelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
+        // Possible other channel name or nickname
         const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
                 : null
-        logMessage([`> checkSelfSub(chatroom: '${chatroom}', otherChannel: '${otherChannel}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}')`])
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
+        logMessage([`> checkSelfSub(chatroom: '${chatroom}', userNickname: '${userNickname}', channel: '${channel}', validatedOtherChannelName: '${validatedOtherChannelName}')`])
 
         const hypeEmote = getHypeEmote(channel)
         const negativeEmote = getNegativeEmote(channel)
         const dumbEmote = getDumbEmote(channel)
 
-        otherChannelNickname
-            ? otherChannel in user
-                ? user[otherChannel].sub
+        validatedOtherChannelName
+            ? validatedOtherChannelName in user
+                ? user[validatedOtherChannelName].sub
                     ? bot.say(chatroom, `Yes ${userNickname}, you are subbed to ${otherChannelNickname}! ${hypeEmote}`)
                     : bot.say(chatroom, `No ${userNickname}, you are not subbed to ${otherChannelNickname}! ${negativeEmote}`)
                 : bot.say(chatroom, `I don't know whether you are subbed to ${otherChannelNickname}, ${userNickname}! ${dumbEmote}`)
             : user[channel].sub
-                ? bot.say(chatroom, `Yes ${userNickname}, you are subbed to ${channelNickname}! ${hypeEmote}`)
-                : bot.say(chatroom, `No ${userNickname}, you are not subbed to ${channelNickname}! ${negativeEmote}`)
+                ? bot.say(chatroom, `Yes ${userNickname}, you are subbed to ${currentChannelNickname}! ${hypeEmote}`)
+                : bot.say(chatroom, `No ${userNickname}, you are not subbed to ${currentChannelNickname}! ${negativeEmote}`)
     },
     checkSelfMod(props, splitMessage) {
         const { bot, chatroom, channel, user, userNickname } = props
-        const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
+        const currentChannelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
+        // Possible other channel name or nickname
         const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
                 : null
-        logMessage([`> checkSelfMod(chatroom: '${chatroom}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}')`])
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
+        logMessage([`> checkSelfMod(chatroom: '${chatroom}', userNickname: '${userNickname}', channel: '${channel}', validatedOtherChannelName: '${validatedOtherChannelName}')`])
 
         const hypeEmote = getHypeEmote(channel)
         const negativeEmote = getNegativeEmote(channel)
         const dumbEmote = getDumbEmote(channel)
 
-        otherChannelNickname
-            ? otherChannel in user
-                ? user[otherChannel].mod
+        validatedOtherChannelName
+            ? validatedOtherChannelName in user
+                ? user[validatedOtherChannelName].mod
                     ? bot.say(chatroom, `Yes ${userNickname}, you are a mod in ${otherChannelNickname}'s chat! ${hypeEmote}`)
                     : bot.say(chatroom, `No ${userNickname}, you are not a mod in ${otherChannelNickname}'s chat! ${negativeEmote}`)
                 : bot.say(chatroom, `I don't know whether you are a mod in ${otherChannelNickname}'s chat, ${userNickname}! ${dumbEmote}`)
             : user[channel].mod
-                ? bot.say(chatroom, `Yes ${userNickname}, you are a mod in ${channelNickname}'s chat! ${hypeEmote}`)
-                : bot.say(chatroom, `No ${userNickname}, you are not a mod in ${channelNickname}'s chat! ${negativeEmote}`)
+                ? bot.say(chatroom, `Yes ${userNickname}, you are a mod in ${currentChannelNickname}'s chat! ${hypeEmote}`)
+                : bot.say(chatroom, `No ${userNickname}, you are not a mod in ${currentChannelNickname}'s chat! ${negativeEmote}`)
     },
     checkSelfVIP(props, splitMessage) {
         const { bot, chatroom, channel, user, userNickname } = props
-        const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
+        const currentChannelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
+        // Possible other channel name or nickname
         const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
                 : null
-        logMessage([`> checkSelfVIP(chatroom: '${chatroom}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}')`])
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
+        logMessage([`> checkSelfVIP(chatroom: '${chatroom}', userNickname: '${userNickname}', channel: '${channel}', validatedOtherChannelName: '${validatedOtherChannelName}')`])
 
         const hypeEmote = getHypeEmote(channel)
         const negativeEmote = getNegativeEmote(channel)
         const dumbEmote = getDumbEmote(channel)
 
-        otherChannelNickname
-            ? otherChannel in user
-                ? user[otherChannel].vip
+        validatedOtherChannelName
+            ? validatedOtherChannelName in user
+                ? user[validatedOtherChannelName].vip
                     ? bot.say(chatroom, `Yes ${userNickname}, you are a VIP in ${otherChannelNickname}'s chat! ${hypeEmote}`)
                     : bot.say(chatroom, `No ${userNickname}, you are not a VIP in ${otherChannelNickname}'s chat! ${negativeEmote}`)
                 : bot.say(chatroom, `I don't know whether you are a VIP in ${otherChannelNickname}'s chat, ${userNickname}! ${dumbEmote}`)
             : user[channel].vip
-                ? bot.say(chatroom, `Yes ${userNickname}, you are a VIP in ${channelNickname}'s chat! ${hypeEmote}`)
-                : bot.say(chatroom, `No ${userNickname}, you are not a VIP in ${channelNickname}'s chat! ${negativeEmote}`)
+                ? bot.say(chatroom, `Yes ${userNickname}, you are a VIP in ${currentChannelNickname}'s chat! ${hypeEmote}`)
+                : bot.say(chatroom, `No ${userNickname}, you are not a VIP in ${currentChannelNickname}'s chat! ${negativeEmote}`)
     },
     checkTargetSub(props, splitMessage) {
         const { bot, chatroom, channel, userNickname } = props
-        const targetUser = getToUser(splitMessage[1])
-        const target = users[targetUser] || null
-        const targetNickname = target?.nickname || target?.displayName || null
-        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
         const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        // Parse target user
+        const toUser = getToUser(splitMessage[1])
+        const toUserByNickname = findUserByNickname(toUser)
+        const target = toUserByNickname in users
+            ? users[toUserByNickname]
+            : toUser in users
+                ? users[toUser]
                 : null
+        const targetNickname = target?.nickname || target?.displayName || null
+
+        // Possible other channel name or nickname
+        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
+                : null
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
         logMessage([`> checkTargetSub(chatroom: '${chatroom}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}', targetNickname: '${targetNickname}')`])
 
         const hypeEmote = getHypeEmote(channel)
@@ -115,9 +141,9 @@ module.exports = {
         const dumbEmote = getDumbEmote(channel)
 
         if (target) {
-            otherChannelNickname
-                ? otherChannel in target
-                    ? target[otherChannel].sub
+            validatedOtherChannelName
+                ? validatedOtherChannelName in target
+                    ? target[validatedOtherChannelName].sub
                         ? bot.say(chatroom, `Yes ${userNickname}, ${targetNickname} is subbed to ${otherChannelNickname}! ${hypeEmote}`)
                         : bot.say(chatroom, `No ${userNickname}, ${targetNickname} is not subbed to ${otherChannelNickname}! ${negativeEmote}`)
                     : bot.say(chatroom, `I don't know whether ${targetNickname} is subbed in ${otherChannelNickname}'s chat, ${userNickname}! ${dumbEmote}`)
@@ -130,18 +156,28 @@ module.exports = {
     },
     checkTargetMod(props, splitMessage) {
         const { bot, chatroom, channel, userNickname } = props
-        const targetUser = getToUser(splitMessage[1])
-        const target = users[targetUser] || null
-        const targetNickname = target?.nickname || target?.displayName || null
-        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
         const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        // Parse target user
+        const toUser = getToUser(splitMessage[1])
+        const toUserByNickname = findUserByNickname(toUser)
+        const target = toUserByNickname in users
+            ? users[toUserByNickname]
+            : toUser in users
+                ? users[toUser]
                 : null
+        const targetNickname = target?.nickname || target?.displayName || null
+
+        // Possible other channel name or nickname
+        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
+                : null
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
         logMessage([`> checkTargetMod(chatroom: '${chatroom}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}', targetNickname: '${targetNickname}')`])
 
         const hypeEmote = getHypeEmote(channel)
@@ -149,9 +185,9 @@ module.exports = {
         const dumbEmote = getDumbEmote(channel)
 
         if (target) {
-            otherChannelNickname
-                ? otherChannel in target
-                    ? target[otherChannel].mod
+            validatedOtherChannelName
+                ? validatedOtherChannelName in target
+                    ? target[validatedOtherChannelName].mod
                         ? bot.say(chatroom, `Yes ${userNickname}, ${targetNickname} is a mod in ${otherChannelNickname}'s chat! ${hypeEmote}`)
                         : bot.say(chatroom, `No ${userNickname}, ${targetNickname} is not a mod in ${otherChannelNickname}'s chat! ${negativeEmote}`)
                     : bot.say(chatroom, `I don't know whether ${targetNickname} is a mod in ${otherChannelNickname}'s chat, ${userNickname}! ${dumbEmote}`)
@@ -164,18 +200,28 @@ module.exports = {
     },
     checkTargetVIP(props, splitMessage) {
         const { bot, chatroom, channel, userNickname } = props
-        const targetUser = getToUser(splitMessage[1])
-        const target = users[targetUser] || null
-        const targetNickname = target?.nickname || target?.displayName || null
-        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
         const channelNickname = users[channel]?.nickname || users[channel]?.displayName || channel
 
-        const possibleNickname = findUserByNickname(otherChannel)
-        const otherChannelNickname = otherChannel in lemonyFresh
-            ? users[otherChannel]?.nickname || users[otherChannel]?.displayName || otherChannel
-            : possibleNickname in lemonyFresh
-                ? users[possibleNickname]?.nickname || users[possibleNickname]?.displayName || possibleNickname
+        // Parse target user
+        const toUser = getToUser(splitMessage[1])
+        const toUserByNickname = findUserByNickname(toUser)
+        const target = toUserByNickname in users
+            ? users[toUserByNickname]
+            : toUser in users
+                ? users[toUser]
                 : null
+        const targetNickname = target?.nickname || target?.displayName || null
+
+        // Possible other channel name or nickname
+        const otherChannel = getToUser(splitMessage[splitMessage.length - 1].split(` `)[0].replace(/'s|\?/g, ``))
+        const otherChannelByNickname = findUserByNickname(otherChannel)
+        const validatedOtherChannelName = otherChannelByNickname in lemonyFresh
+            ? otherChannelByNickname
+            : otherChannel in lemonyFresh
+                ? otherChannel
+                : null
+        const otherChannelNickname = users[validatedOtherChannelName]?.nickname || users[validatedOtherChannelName]?.displayName || validatedOtherChannelName
+
         logMessage([`> checkTargetVIP(chatroom: '${chatroom}', otherChannelNickname: '${otherChannelNickname}', userNickname: '${userNickname}', targetNickname: '${targetNickname}')`])
 
         const hypeEmote = getHypeEmote(channel)
@@ -183,9 +229,9 @@ module.exports = {
         const dumbEmote = getDumbEmote(channel)
 
         if (target) {
-            otherChannelNickname
-                ? otherChannel in target
-                    ? target[otherChannel].vip
+            validatedOtherChannelName
+                ? validatedOtherChannelName in target
+                    ? target[validatedOtherChannelName].vip
                         ? bot.say(chatroom, `Yes ${userNickname}, ${targetNickname} is a VIP in ${otherChannelNickname}'s chat! ${hypeEmote}`)
                         : bot.say(chatroom, `No ${userNickname}, ${targetNickname} is not a VIP in ${otherChannelNickname}'s chat! ${negativeEmote}`)
                     : bot.say(chatroom, `I don't know whether ${targetNickname} is a VIP in ${otherChannelNickname}'s chat, ${userNickname}! ${dumbEmote}`)
