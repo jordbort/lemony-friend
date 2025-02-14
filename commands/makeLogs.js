5cba41081b0f70c5eead39e9b7f99205e3a2bf34
jordbort <jordan.bortner@gmail.com>
2025-02-14 15:08:22 -0500

    Refactor memory dump

--- commands/makeLogs.js
+++ commands/makeLogs.js
@@ -1,37 +1,11 @@
-const BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN
-
-const JPEGSTRIPES_ACCESS_TOKEN = process.env.JPEGSTRIPES_ACCESS_TOKEN
-const JPEGSTRIPES_REFRESH_TOKEN = process.env.JPEGSTRIPES_REFRESH_TOKEN
-
-const SCLARF_ACCESS_TOKEN = process.env.SCLARF_ACCESS_TOKEN
-const SCLARF_REFRESH_TOKEN = process.env.SCLARF_REFRESH_TOKEN
-
-const E1ECTROMA_ACCESS_TOKEN = process.env.E1ECTROMA_ACCESS_TOKEN
-const E1ECTROMA_REFRESH_TOKEN = process.env.E1ECTROMA_REFRESH_TOKEN
-
-const DOMONINTENDO1_ACCESS_TOKEN = process.env.DOMONINTENDO1_ACCESS_TOKEN
-const DOMONINTENDO1_REFRESH_TOKEN = process.env.DOMONINTENDO1_REFRESH_TOKEN
-
-const PPUYYA_ACCESS_TOKEN = process.env.PPUYYA_ACCESS_TOKEN
-const PPUYYA_REFRESH_TOKEN = process.env.PPUYYA_REFRESH_TOKEN
-
-const ASTRAL_AN0MALY_ACCESS_TOKEN = process.env.ASTRAL_AN0MALY_ACCESS_TOKEN
-const ASTRAL_AN0MALY_REFRESH_TOKEN = process.env.ASTRAL_AN0MALY_REFRESH_TOKEN
-
-const DIRTYD0INKS_ACCESS_TOKEN = process.env.DIRTYD0INKS_ACCESS_TOKEN
-const DIRTYD0INKS_REFRESH_TOKEN = process.env.DIRTYD0INKS_REFRESH_TOKEN
-
-const ARTYINPINK_ACCESS_TOKEN = process.env.ARTYINPINK_ACCESS_TOKEN
-const ARTYINPINK_REFRESH_TOKEN = process.env.ARTYINPINK_REFRESH_TOKEN
-
-const THETARASTARK_ACCESS_TOKEN = process.env.THETARASTARK_ACCESS_TOKEN
-const THETARASTARK_REFRESH_TOKEN = process.env.THETARASTARK_REFRESH_TOKEN
-
-const CATJERKY_ACCESS_TOKEN = process.env.CATJERKY_ACCESS_TOKEN
-const CATJERKY_REFRESH_TOKEN = process.env.CATJERKY_REFRESH_TOKEN
-
-const SKITTLE108_ACCESS_TOKEN = process.env.SKITTLE108_ACCESS_TOKEN
-const SKITTLE108_REFRESH_TOKEN = process.env.SKITTLE108_REFRESH_TOKEN
+const BOT_USERNAME = process.env.BOT_USERNAME
+const BOT_ID = process.env.BOT_ID
+const DEV = process.env.DEV
+const OAUTH_TOKEN = process.env.OAUTH_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI
+const API_KEY = process.env.API_KEY
 
 const { settings, wordBank } = require(`../config`)
 const { lemonyFresh, mods, users, knownTags, tempCmds, commonNicknames, startingLemons, hangmanWins } = require(`../data`)
@@ -61,34 +35,14 @@ function renderObj(obj, objName, indentation = ``) {
     return data.join(``)
 }
 
-function tokenChangeWarning() {
-    const starBorder = `${Array(50).fill(`*`).join(` `)}\n`
-    let msgBlock = starBorder
-    if (lemonyFresh.botAccessToken !== BOT_ACCESS_TOKEN) { msgBlock += `BOT_ACCESS_TOKEN changed, update to: '${lemonyFresh.botAccessToken}'\n` }
-    if (lemonyFresh.jpegstripes.accessToken !== JPEGSTRIPES_ACCESS_TOKEN) { msgBlock += `JPEGSTRIPES_ACCESS_TOKEN changed, update to: '${lemonyFresh.jpegstripes.accessToken}'\n` }
-    if (lemonyFresh.jpegstripes.refreshToken !== JPEGSTRIPES_REFRESH_TOKEN) { msgBlock += `JPEGSTRIPES_REFRESH_TOKEN changed, update to: '${lemonyFresh.jpegstripes.refreshToken}'\n` }
-    if (lemonyFresh.sclarf.accessToken !== SCLARF_ACCESS_TOKEN) { msgBlock += `SCLARF_ACCESS_TOKEN changed, update to: '${lemonyFresh.sclarf.accessToken}'\n` }
-    if (lemonyFresh.sclarf.refreshToken !== SCLARF_REFRESH_TOKEN) { msgBlock += `SCLARF_REFRESH_TOKEN changed, update to: '${lemonyFresh.sclarf.refreshToken}'\n` }
-    if (lemonyFresh.e1ectroma.accessToken !== E1ECTROMA_ACCESS_TOKEN) { msgBlock += `E1ECTROMA_ACCESS_TOKEN changed, update to: '${lemonyFresh.e1ectroma.accessToken}'\n` }
-    if (lemonyFresh.e1ectroma.refreshToken !== E1ECTROMA_REFRESH_TOKEN) { msgBlock += `E1ECTROMA_REFRESH_TOKEN changed, update to: '${lemonyFresh.e1ectroma.refreshToken}'\n` }
-    if (lemonyFresh.domonintendo1.accessToken !== DOMONINTENDO1_ACCESS_TOKEN) { msgBlock += `DOMONINTENDO1_ACCESS_TOKEN changed, update to: '${lemonyFresh.domonintendo1.accessToken}'\n` }
-    if (lemonyFresh.domonintendo1.refreshToken !== DOMONINTENDO1_REFRESH_TOKEN) { msgBlock += `DOMONINTENDO1_REFRESH_TOKEN changed, update to: '${lemonyFresh.domonintendo1.refreshToken}'\n` }
-    if (lemonyFresh.ppuyya.accessToken !== PPUYYA_ACCESS_TOKEN) { msgBlock += `PPUYYA_ACCESS_TOKEN changed, update to: '${lemonyFresh.ppuyya.accessToken}'\n` }
-    if (lemonyFresh.ppuyya.refreshToken !== PPUYYA_REFRESH_TOKEN) { msgBlock += `PPUYYA_REFRESH_TOKEN changed, update to: '${lemonyFresh.ppuyya.refreshToken}'\n` }
-    if (lemonyFresh.astral_an0maly.accessToken !== ASTRAL_AN0MALY_ACCESS_TOKEN) { msgBlock += `ASTRAL_AN0MALY_ACCESS_TOKEN changed, update to: '${lemonyFresh.astral_an0maly.accessToken}'\n` }
-    if (lemonyFresh.astral_an0maly.refreshToken !== ASTRAL_AN0MALY_REFRESH_TOKEN) { msgBlock += `ASTRAL_AN0MALY_REFRESH_TOKEN changed, update to: '${lemonyFresh.astral_an0maly.refreshToken}'\n` }
-    if (lemonyFresh.dirtyd0inks.accessToken !== DIRTYD0INKS_ACCESS_TOKEN) { msgBlock += `DIRTYD0INKS_ACCESS_TOKEN changed, update to: '${lemonyFresh.dirtyd0inks.accessToken}'\n` }
-    if (lemonyFresh.dirtyd0inks.refreshToken !== DIRTYD0INKS_REFRESH_TOKEN) { msgBlock += `DIRTYD0INKS_REFRESH_TOKEN changed, update to: '${lemonyFresh.dirtyd0inks.refreshToken}'\n` }
-    if (lemonyFresh.artyinpink.accessToken !== ARTYINPINK_ACCESS_TOKEN) { msgBlock += `ARTYINPINK_ACCESS_TOKEN changed, update to: '${lemonyFresh.artyinpink.accessToken}'\n` }
-    if (lemonyFresh.artyinpink.refreshToken !== ARTYINPINK_REFRESH_TOKEN) { msgBlock += `ARTYINPINK_REFRESH_TOKEN changed, update to: '${lemonyFresh.artyinpink.refreshToken}'\n` }
-    if (lemonyFresh.thetarastark.accessToken !== THETARASTARK_ACCESS_TOKEN) { msgBlock += `THETARASTARK_ACCESS_TOKEN changed, update to: '${lemonyFresh.thetarastark.accessToken}'\n` }
-    if (lemonyFresh.thetarastark.refreshToken !== THETARASTARK_REFRESH_TOKEN) { msgBlock += `THETARASTARK_REFRESH_TOKEN changed, update to: '${lemonyFresh.thetarastark.refreshToken}'\n` }
-    if (mods.catjerky.accessToken !== CATJERKY_ACCESS_TOKEN) { msgBlock += `CATJERKY_ACCESS_TOKEN changed, update to: '${mods.catjerky.accessToken}'\n` }
-    if (mods.catjerky.refreshToken !== CATJERKY_REFRESH_TOKEN) { msgBlock += `CATJERKY_REFRESH_TOKEN changed, update to: '${mods.catjerky.refreshToken}'\n` }
-    if (mods.skittle108.accessToken !== SKITTLE108_ACCESS_TOKEN) { msgBlock += `SKITTLE108_ACCESS_TOKEN changed, update to: '${mods.skittle108.accessToken}'\n` }
-    if (mods.skittle108.refreshToken !== SKITTLE108_REFRESH_TOKEN) { msgBlock += `SKITTLE108_REFRESH_TOKEN changed, update to: '${mods.skittle108.refreshToken}'\n` }
-    msgBlock += starBorder
-    return msgBlock
+function createCSV(prevObj, currObj, property) {
+    const listCSV = Object.keys(currObj)
+        .map(key => { return [key, currObj[key][property]] })
+        .filter(el => el[1])
+        .concat(Object.keys(prevObj).map(key => { return [key, prevObj[key]] }))
+        .sort()
+        .join()
+    return listCSV
 }
 
 function makeLogs(arr) {
@@ -126,43 +80,66 @@ function makeLogs(arr) {
         logs += `${renderObj(obj, objName)}\n\n`
     }
 
-    const listUserNicknames = Object.keys(users)
-        .map(user => { return [user, users[user].nickname] })
-        .filter(el => el[1])
-        .concat(Object.keys(commonNicknames).map(key => { return [key, commonNicknames[key]] }))
-        .sort()
-        .join()
-    logs += `COMMON_NICKNAMES='${listUserNicknames}'\n\n`
+    logs += `${Array(50).fill(`*`).join(` `)}`
 
-    const listUserLemons = Object.keys(users)
-        .map(user => { return [user, users[user].lemons] })
-        .filter(el => el[1])
-        .concat(Object.keys(startingLemons).map(key => { return [key, startingLemons[key]] }))
-        .sort()
-        .join()
-    logs += `STARTING_LEMONS='${listUserLemons}'\n\n`
+    logs += `
+BOT_USERNAME='${BOT_USERNAME}'
+BOT_ID='${BOT_ID}'
+DEV='${DEV}'
 
-    // If any tokens have changed, print a footer
-    const anyTokenChange = lemonyFresh.botAccessToken !== BOT_ACCESS_TOKEN
-        || lemonyFresh.jpegstripes.accessToken !== JPEGSTRIPES_ACCESS_TOKEN
-        || lemonyFresh.jpegstripes.refreshToken !== JPEGSTRIPES_REFRESH_TOKEN
+OAUTH_TOKEN='${OAUTH_TOKEN}'
 
-        || lemonyFresh.sclarf.accessToken !== SCLARF_ACCESS_TOKEN
-        || lemonyFresh.sclarf.refreshToken !== SCLARF_REFRESH_TOKEN
+CLIENT_ID='${CLIENT_ID}'
+CLIENT_SECRET='${CLIENT_SECRET}'
+BOT_ACCESS_TOKEN='${lemonyFresh.botAccessToken}'
 
-        || lemonyFresh.e1ectroma.accessToken !== E1ECTROMA_ACCESS_TOKEN
-        || lemonyFresh.e1ectroma.refreshToken !== E1ECTROMA_REFRESH_TOKEN
+REDIRECT_URI='${REDIRECT_URI}'
 
-        || lemonyFresh.domonintendo1.accessToken !== DOMONINTENDO1_ACCESS_TOKEN
-        || lemonyFresh.domonintendo1.refreshToken !== DOMONINTENDO1_REFRESH_TOKEN
+API_KEY='${API_KEY}'
 
-        || lemonyFresh.dirtyd0inks.accessToken !== DIRTYD0INKS_ACCESS_TOKEN
-        || lemonyFresh.dirtyd0inks.refreshToken !== DIRTYD0INKS_REFRESH_TOKEN
+JPEGSTRIPES_ACCESS_TOKEN='${lemonyFresh.jpegstripes.accessToken}'
+JPEGSTRIPES_REFRESH_TOKEN='${lemonyFresh.jpegstripes.refreshToken}'
 
-        || mods.catjerky.accessToken !== CATJERKY_ACCESS_TOKEN
-        || mods.catjerky.refreshToken !== CATJERKY_REFRESH_TOKEN
+SCLARF_ACCESS_TOKEN='${lemonyFresh.sclarf.accessToken}'
+SCLARF_REFRESH_TOKEN='${lemonyFresh.sclarf.refreshToken}'
 
-    if (anyTokenChange) { logs += tokenChangeWarning() }
+E1ECTROMA_ACCESS_TOKEN='${lemonyFresh.e1ectroma.accessToken}'
+E1ECTROMA_REFRESH_TOKEN='${lemonyFresh.e1ectroma.refreshToken}'
+
+DOMONINTENDO1_ACCESS_TOKEN='${lemonyFresh.domonintendo1.accessToken}'
+DOMONINTENDO1_REFRESH_TOKEN='${lemonyFresh.domonintendo1.refreshToken}'
+
+PPUYYA_ACCESS_TOKEN='${lemonyFresh.ppuyya.accessToken}'
+PPUYYA_REFRESH_TOKEN='${lemonyFresh.ppuyya.refreshToken}'
+
+ASTRAL_AN0MALY_ACCESS_TOKEN='${lemonyFresh.astral_an0maly.accessToken}'
+ASTRAL_AN0MALY_REFRESH_TOKEN='${lemonyFresh.astral_an0maly.refreshToken}'
+
+DIRTYD0INKS_ACCESS_TOKEN='${lemonyFresh.dirtyd0inks.accessToken}'
+DIRTYD0INKS_REFRESH_TOKEN='${lemonyFresh.dirtyd0inks.refreshToken}'
+
+ARTYINPINK_ACCESS_TOKEN='${lemonyFresh.artyinpink.accessToken}'
+ARTYINPINK_REFRESH_TOKEN='${lemonyFresh.artyinpink.refreshToken}'
+
+THETARASTARK_ACCESS_TOKEN='${lemonyFresh.thetarastark.accessToken}'
+THETARASTARK_REFRESH_TOKEN='${lemonyFresh.thetarastark.refreshToken}'
+
+SLEBTV_ACCESS_TOKEN='${lemonyFresh.slebtv.accessToken}'
+SLEBTV_REFRESH_TOKEN='${lemonyFresh.slebtv.refreshToken}'
+
+CATJERKY_ACCESS_TOKEN='${mods.catjerky.accessToken}'
+CATJERKY_REFRESH_TOKEN='${mods.catjerky.refreshToken}'
+
+SKITTLE108_ACCESS_TOKEN='${mods.skittle108.accessToken}'
+SKITTLE108_REFRESH_TOKEN='${mods.skittle108.refreshToken}'\n\n`
+
+    logs += `COMMON_NICKNAMES='${createCSV(commonNicknames, users, `nickname`)}'\n\n`
+
+    logs += `STARTING_LEMONS='${createCSV(startingLemons, users, `lemons`)}'\n\n`
+
+    logs += `HANGMAN_WINS='${createCSV(hangmanWins, users, `hangmanWins`)}'\n\n`
+
+    logs += `${Array(50).fill(`*`).join(` `)}\n`
 
     return logs
 }
