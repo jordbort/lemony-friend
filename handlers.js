7ca3466efec1000caeebbce653964d72bc47b536
jordbort <jordan.bortner@gmail.com>
2025-03-07 00:50:36 -0500

    Sanitize single quotes

--- handlers.js
+++ handlers.js
@@ -63,7 +63,7 @@ module.exports = {
             return
         }
 
-        const msg = message.replace(/ +/g, ` `)
+        const msg = message.replace(/ +/g, ` `).replace(`'`, `’`)
         const channel = chatroom.substring(1)
         const username = tags.username
         const time = new Date().toLocaleTimeString(settings.timeLocale, { timeZone: settings.timeZone })
