b8abf80803c2717a8e24aff3f66d991ccdda6e42
jordbort <jordan.bortner@gmail.com>
2025-03-24 02:10:40 -0400

    Implement memory.json

--- data.js
+++ data.js
@@ -1,26 +1,6 @@
-const lemonyFresh = JSON.parse(process.env.LEMONY_FRESH)
-const mods = JSON.parse(process.env.MOD_DATA)
-const users = JSON.parse(process.env.USER_DATA)
-const knownTags = JSON.parse(process.env.KNOWN_TAGS)
-const lemCmds = JSON.parse(process.env.LEMON_COMMANDS)
-
-const COMMON_NICKNAMES = process.env.COMMON_NICKNAMES
-const commonNicknames = {}
-for (let arr = COMMON_NICKNAMES.split(`,`), i = 0; i < arr.length; i++) { if (i % 2 === 0) { commonNicknames[arr[i]] = arr[i + 1] } }
-
-const STARTING_LEMONS = process.env.STARTING_LEMONS
-const startingLemons = {}
-for (let arr = STARTING_LEMONS.split(`,`), i = 0; i < arr.length; i++) { if (i % 2 === 0) { startingLemons[arr[i]] = Number(arr[i + 1]) } }
-
-const HANGMAN_WINS = process.env.HANGMAN_WINS
-const hangmanWins = {}
-for (let arr = HANGMAN_WINS.split(`,`), i = 0; i < arr.length; i++) { if (i % 2 === 0) { hangmanWins[arr[i]] = Number(arr[i + 1]) } }
-
-const wordBank = {
-    nouns: JSON.parse(process.env.WORDBANK_NOUNS),
-    verbs: JSON.parse(process.env.WORDBANK_VERBS),
-    adjectives: JSON.parse(process.env.WORDBANK_ADJECTIVES)
-}
+const { lemonyFresh, mods, users, knownTags, lemCmds, wordBank,
+    commonNicknames, startingLemons, hangmanWins // unclaimed legacy memory tables
+} = require(`./memory.json`)
 
 module.exports = {
     lemonyFresh,
@@ -28,8 +8,8 @@ module.exports = {
     users,
     knownTags,
     lemCmds,
+    wordBank,
     commonNicknames,
     startingLemons,
-    hangmanWins,
-    wordBank
+    hangmanWins
 }
