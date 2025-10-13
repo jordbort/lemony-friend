23de415516e5f5db67db901cbe124d9081d143ae
jordbort <jordan.bortner@gmail.com>
2025-10-13 16:04:50 -0400

    Abstract logic for onMessage handler

--- handlers.js
+++ handlers.js
@@ -21,6 +21,94 @@ const { handleNewChatter, welcomeBack, reportAway, funTimerGuess } = require(`./
 const { handleColorChange, handleSubChange, handleModChange, handleVIPChange } = require(`./commands/userChange`)
 const { initUser, initUserChannel, initChannel, updateMod, getToUser, tagsListener, logMessage, acknowledgeGigantifiedEmote, appendLogs } = require(`./utils`)
 
+function devCommandUsed(props) {
+    const { username, command } = props
+    if (username === DEV) {
+        if (command in dev) {
+            logMessage([`MATCHED DEV COMMAND:`, command, `[Function: ${dev[command].name}]`])
+            dev[command](props)
+            return true
+        }
+    }
+    return false
+}
+function commandUsed(props) {
+    const { message, command } = props
+    if (message.startsWith(`!`)) {
+        if (command in commands) {
+            logMessage([`MATCHED COMMAND:`, command, `[Function: ${commands[command].name}]`])
+            commands[command](props)
+            return true
+        } else if (/!.+lemon/i.test(command) in lemCmds) { logMessage([`COMMAND NOT RECOGNIZED`]) }
+    }
+    return false
+}
+function regexMatch(props) {
+    const { message } = props
+    for (const pattern in patterns) {
+        const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
+        if (regex.test(message)) {
+            logMessage([`MESSAGE MATCHED REGEX PATTERN:`, regex, `[Function: ${patterns[regex].name}]`])
+            patterns[regex](props, message.split(regex))
+            return true
+        }
+    }
+    return false
+}
+function botMentionedByBot(props) {
+    const { bot, chatroom, username, message, channel } = props
+    if ([`streamelements`, `pokemoncommunitygame`].includes(username)) {
+        if (message.includes(BOT_USERNAME)) {
+            for (const pattern in botInteraction) {
+                const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
+                if (regex.test(message)) {
+                    logMessage([`${username.toUpperCase()} MATCHED REGEX PATTERN:`, regex, `[Function: ${botInteraction[regex].name}]`])
+                    botInteraction[regex](props, message.split(regex))
+                    return true
+                }
+            }
+            logMessage([`${username.toUpperCase()} DID NOT MATCH REGEX PATTERNS`])
+        }
+        if (!(`points` in Object(users[BOT_USERNAME].channels[channel])) && username === `streamelements`) {
+            users[BOT_USERNAME].channels[channel].points = 0
+            bot.say(chatroom, `!points`)
+            return true
+        }
+    }
+    return false
+}
+function botMentioned(props) {
+    const { message } = props
+    if (RegExp(`\\b${BOT_NICKNAME_REGEX}\\b`, `i`).test(message)) {
+        for (const pattern in botMention) {
+            const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
+            if (regex.test(message)) {
+                logMessage([`BOT MENTION MATCHED REGEX PATTERN:`, regex, `[Function: ${botMention[regex].name}]`])
+                botMention[regex](props, message.split(regex))
+                return true
+            }
+        }
+        logMessage([`BOT MENTION DID NOT MATCH REGEX PATTERNS`])
+    }
+    return false
+}
+function hangmanListener(props) {
+    const { message, channel, username } = props
+    const hangman = lemonyFresh[channel].hangman
+    if (hangman.listening && !hangman.signup && username === hangman.players[hangman.currentPlayer]) {
+        if (/^[a-z]$/i.test(message)) {
+            checkLetter(props)
+            return true
+        }
+        if (/^[a-z]{2,}$/i.test(message) && message.length === hangman.answer.length) {
+            checkWord(props)
+            return true
+        }
+        logMessage([`NOT A HANGMAN GUESS`])
+    }
+    return false
+}
+
 module.exports = {
     onConnectedHandler(addr, port) {
         settings.firstConnection && printLemon()
@@ -158,27 +246,24 @@ module.exports = {
         // Acknowledge highlighted message
         if (tags[`msg-id`] === `highlighted-message`) { logMessage([`This was a highlighted message`]) }
 
-        /**************\
-        !COMMANDS PARSER
-        \**************/
-        // Dev commands
-        if (username === DEV) {
-            if (command in dev) {
-                logMessage([`MATCHED DEV COMMAND:`, command, `[Function: ${dev[command].name}]`])
-                dev[command](props)
-                return
-            }
-        }
-        if (msg.startsWith(`!`)) {
-            if (command in commands) {
-                logMessage([`MATCHED COMMAND:`, command, `[Function: ${commands[command].name}]`])
-                commands[command](props)
-                return
-            } else if (/!.+lemon/i.test(command) in lemCmds) { logMessage([`COMMAND NOT RECOGNIZED`]) }
-        }
+        // Commands listener
+        if (devCommandUsed(props)) { return }
+        if (commandUsed(props)) { return }
 
         // Check user's first message in a given channel
-        if (tags[`first-msg`]) { return handleNewChatter(props) }
+        if (tags[`first-msg`]) {
+            handleNewChatter(props)
+            return
+        }
+
+        // General regex patterns
+        if (regexMatch(props)) { return }
+
+        // Other bot mentions bot
+        if (botMentionedByBot(props)) { return }
+
+        // Any user mentions bot
+        if (botMentioned(props)) { return }
 
         // User is the funTimerGuesser and making an attempt
         if (lemonyFresh[channel].funTimerGuesser === username && /\b\d+\b/.test(msg)) {
@@ -186,65 +271,8 @@ module.exports = {
             return
         }
 
-        /***********\
-        REGEX PARSERS
-        \***********/
-        // General regex patterns
-        for (const pattern in patterns) {
-            const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
-            if (regex.test(msg)) {
-                logMessage([`MESSAGE MATCHED REGEX PATTERN:`, regex, `[Function: ${patterns[regex].name}]`])
-                patterns[regex](props, msg.split(regex))
-                return
-            }
-        }
-
-        // Other bot mentions bot
-        if ([`streamelements`, `pokemoncommunitygame`].includes(username)) {
-            if (msg.includes(BOT_USERNAME)) {
-                for (const pattern in botInteraction) {
-                    const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
-                    if (regex.test(msg)) {
-                        logMessage([`${username.toUpperCase()} MATCHED REGEX PATTERN:`, regex, `[Function: ${botInteraction[regex].name}]`])
-                        botInteraction[regex](props, msg.split(regex))
-                        return
-                    }
-                }
-                logMessage([`${username.toUpperCase()} DID NOT MATCH REGEX PATTERNS`])
-            }
-            if (!(`points` in Object(users[BOT_USERNAME].channels[channel])) && username === `streamelements`) {
-                users[BOT_USERNAME].channels[channel].points = 0
-                this.say(chatroom, `!points`)
-                return
-            }
-        }
-
-        // Any user mentions bot
-        if (RegExp(`\\b${BOT_NICKNAME_REGEX}\\b`, `i`).test(msg)) {
-            for (const pattern in botMention) {
-                const regex = new RegExp(pattern.split(`/`)[1], pattern.split(`/`)[2])
-                if (regex.test(msg)) {
-                    logMessage([`BOT MENTION MATCHED REGEX PATTERN:`, regex, `[Function: ${botMention[regex].name}]`])
-                    botMention[regex](props, msg.split(regex))
-                    return
-                }
-            }
-            logMessage([`BOT MENTION DID NOT MATCH REGEX PATTERNS`])
-        }
-
         // If hangman has started, and it's the current player's turn
-        const hangman = lemonyFresh[channel].hangman
-        if (hangman.listening && !hangman.signup && username === hangman.players[hangman.currentPlayer]) {
-            if (/^[a-z]$/i.test(msg)) {
-                checkLetter(props)
-                return
-            }
-            if (/^[a-z]{2,}$/i.test(msg) && msg.length === hangman.answer.length) {
-                checkWord(props)
-                return
-            }
-            logMessage([`NOT A HANGMAN GUESS`])
-        }
+        if (hangmanListener(props)) { return }
 
         // Check for lemonCommand
         if (command in lemCmds) {
