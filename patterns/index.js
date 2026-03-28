04f7a6e07b534275faa853efb3a931b3264c5a59
jordbort <jordan.bortner@gmail.com>
2026-03-28 11:01:57 -0400

    Update RegEx to allow multi-word nicknames

--- patterns/index.js
+++ patterns/index.js
@@ -116,21 +116,31 @@ const splitMessagePatterns = {
     [/^!([a-z]+)lemon([a-z]*)/i]: useLemon,
     [/^cli ?\b/i]: commandLemonInterface,
 
-    [/how many emotes does (@?[a-z0-9_]+) have/i]: checkEmotes,
+    [/how many emotes does (@?[a-z0-9_\s]+) have/i]: checkEmotes,
+    [/how many emotes are in (@?[a-z0-9_\s]+)/i]: checkEmotes,
 
     [/do i (have a )?sub[\w]*( (to|in) )?/i]: checkSelfSub,
     [/am i (a )?sub[\w]*( (to|in) )?/i]: checkSelfSub,
+
     [/do i (have )?mod[\w]*( (for|in) )?/i]: checkSelfMod,
     [/am i (a )?mod[\w]*( (for|in) )?/i]: checkSelfMod,
+
     [/do i (have )?vip[\w]*( (to|in) )?/i]: checkSelfVIP,
     [/am i (a )?vip[\w]*( (to|in) )?/i]: checkSelfVIP,
 
-    [/does (@?[a-z0-9_]+) (have a )?sub[\w]*( (to|in) )?/i]: checkTargetSub,
-    [/is (@?[a-z0-9_]+) (a )?sub[\w]*( (to|in) )?/i]: checkTargetSub,
-    [/does (@?[a-z0-9_]+) (have a )?mod[\w]*( (for|in) )?/i]: checkTargetMod,
-    [/is (@?[a-z0-9_]+) (a )?mod[\w]*( (for|in) )?/i]: checkTargetMod,
-    [/does (@?[a-z0-9_]+) (have )?vip[\w]*( (to|in) )?/i]: checkTargetVIP,
-    [/is (@?[a-z0-9_]+) (a )?vip[\w]*( (to|in) )?/i]: checkTargetVIP
+    [/does (@?[a-z0-9_\s]+) have a sub[\w]*( (to|in) )?/i]: checkTargetSub,
+    [/does (@?[a-z0-9_\s]+) sub[\w]*( (to|in) )?/i]: checkTargetSub,
+    [/is (@?[a-z0-9_\s]+) a sub[\w]*( (to|in) )?/i]: checkTargetSub,
+    [/is (@?[a-z0-9_\s]+) sub[\w]*( (to|in) )?/i]: checkTargetSub,
+
+    [/does (@?[a-z0-9_\s]+) have a mod[\w]*( (for|in) )?/i]: checkTargetMod,
+    [/does (@?[a-z0-9_\s]+) mod[\w]*( (for|in) )?/i]: checkTargetMod,
+    [/is (@?[a-z0-9_\s]+) a mod[\w]*( (for|in) )?/i]: checkTargetMod,
+    [/is (@?[a-z0-9_\s]+) mod[\w]*( (for|in) )?/i]: checkTargetMod,
+
+    [/does (@?[a-z0-9_\s]+) have vip[\w]*( (to|in) )?/i]: checkTargetVIP,
+    [/is (@?[a-z0-9_\s]+) a vip[\w]*( (to|in) )?/i]: checkTargetVIP,
+    [/is (@?[a-z0-9_\s]+) vip[\w]*( (to|in) )?/i]: checkTargetVIP
 }
 
 module.exports = function usePattern(props) {
