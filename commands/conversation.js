f8c85b6c730dcd4c41497f70e15c0072155a28b0
jordbort <jordan.bortner@gmail.com>
2026-04-15 18:24:55 -0400

    Migrate multitwitch command

--- commands/conversation.js
+++ commands/conversation.js
@@ -1,7 +1,7 @@
 const BOT_USERNAME = process.env.BOT_USERNAME
 
 const { settings, users, lemonyFresh } = require(`../data`)
-const { getContextEmote, pluralize, resetCooldownTimer, logMessage, transformText, logArr, containsInaccessibleEmotes } = require(`../utils`)
+const { getContextEmote, pluralize, resetCooldownTimer, logMessage, transformText, logArr, twitchUsernamePattern, containsInaccessibleEmotes } = require(`../utils`)
 
 const { autoBanUser } = require(`./twitch`)
 
@@ -477,6 +477,40 @@ module.exports = {
                     : logMessage([`-> ${targetName} has been away for less than one-minute grace period`])
             }
         }
+    },
+    makeMultiTwitchLink(props) {
+        const { bot, chatroom, args } = props
+        logMessage([`> makeMultiTwitchLink(args: ${logArr(args)})`])
+
+        const channel = chatroom.substring(1)
+        const negativeEmote = getContextEmote(`negative`, channel)
+        const neutralEmote = getContextEmote(`neutral`, channel)
+        const dumbEmote = getContextEmote(`dumb`, channel)
+
+        if (args.length < 2) {
+            bot.say(chatroom, `Please provide at least two usernames! ${neutralEmote}`)
+            return
+        }
+
+        const usernames = args
+            .map(arg => arg.replace(/^[@#]/g, ``).toLowerCase())
+            .filter(arg => twitchUsernamePattern.test(arg))
+            .join(`/`)
+
+        // Check how many valid usernames remain after filtering
+        if (usernames.split(`/`).length < 2) {
+            bot.say(chatroom, `Not enough valid usernames were found! ${dumbEmote}`)
+            return
+        }
+
+        // Check if URL exceeds 500-character limit
+        if (usernames.length > 475) {
+            bot.say(chatroom, `Too many usernames provided! ${negativeEmote}`)
+            return
+        }
+
+        const url = `https://multitwitch.live/${usernames}`
+        bot.say(chatroom, url)
     },
     huge(props) {
         const { bot, chatroom, args } = props
