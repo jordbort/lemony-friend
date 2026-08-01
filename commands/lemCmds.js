b928d773ae31e908fbfe54f66e224c332a85b087
jordbort <jordan.bortner@gmail.com>
2026-08-01 17:07:14 -0400

    Update lemCmd variables

--- commands/lemCmds.js
+++ commands/lemCmds.js
@@ -32,29 +32,53 @@ function applyVariables(str, props) {
     const adjectives = wordBank.adjectives.length ? [...wordBank.adjectives] : [`lemony`, `fresh`]
 
     const newStr = str
+        // {message} or {msg} - Everything after the command name
         .replace(/\{\s?(message|msg)\s?\}/gi, args.join(` `))
+
+        // {user} and {user nn} - User name
         .replace(/\{\s?user\s?\}/gi, users[username].displayName)
+        .replace(/\{\s?user\s?nn\s?\}/gi, users[username].nickname || users[username].displayName)
+
+        // {touser} and {touser nn} - toUser or user name
         .replace(/\{\s?touser\s?\}/gi, users[toUser]?.displayName || args[0] || users[username].displayName)
-        .replace(/\{\s?usernn\s?\}/gi, users[username].nickname || users[username].displayName)
-        .replace(/\{\s?tousernn\s?\}/gi, users[toUser]?.nickname || users[toUser]?.displayName || args[0] || users[username].nickname || users[username].displayName)
-        .replace(/\{\s?streamer\s?\}/gi, channelNickname)
-        .replace(/\{\s?viewer\s?\}/gi, users[randomViewerOne]?.nickname || users[randomViewerOne]?.displayName || randomViewerOne)
-        .replace(/\{\s?viewer\s?1\s?\}/gi, users[randomViewerOne]?.nickname || users[randomViewerOne]?.displayName || randomViewerOne)
-        .replace(/\{\s?viewer\s?2\s?\}/gi, users[randomViewerTwo]?.nickname || users[randomViewerTwo]?.displayName || randomViewerTwo)
-        .replace(/\{\s?viewer\s?3\s?\}/gi, users[randomViewerThree]?.nickname || users[randomViewerThree]?.displayName || randomViewerThree)
+        .replace(/\{\s?touser\s?nn\s?\}/gi, users[toUser]?.nickname || users[toUser]?.displayName || args[0] || users[username].nickname || users[username].displayName)
+
+        // {streamer} and {streamer nn} - Streamer name
+        .replace(/\{\s?streamer\s?\}/gi, users[channel]?.displayName || channel)
+        .replace(/\{\s?streamer\s?nn\s?\}/gi, users[channel]?.nickname || users[channel]?.displayName || channel)
+
+        // {viewer}, and {viewer 1} through {viewer 3} - Random viewer
+        .replace(/\{\s?viewer\s?\}/gi, users[randomViewerOne]?.displayName || randomViewerOne)
+        .replace(/\{\s?viewer\s?1\s?\}/gi, users[randomViewerOne]?.displayName || randomViewerOne)
+        .replace(/\{\s?viewer\s?2\s?\}/gi, users[randomViewerTwo]?.displayName || randomViewerTwo)
+        .replace(/\{\s?viewer\s?3\s?\}/gi, users[randomViewerThree]?.displayName || randomViewerThree)
+
+        // {viewer nn}, and {viewer 1 nn} through {viewer 3 nn} - Random viewer nickname
+        .replace(/\{\s?viewer\s?nn\s?\}/gi, users[randomViewerOne]?.nickname || users[randomViewerOne]?.displayName || randomViewerOne)
+        .replace(/\{\s?viewer\s?1\s?nn\s?\}/gi, users[randomViewerOne]?.nickname || users[randomViewerOne]?.displayName || randomViewerOne)
+        .replace(/\{\s?viewer\s?2\s?nn\s?\}/gi, users[randomViewerTwo]?.nickname || users[randomViewerTwo]?.displayName || randomViewerTwo)
+        .replace(/\{\s?viewer\s?3\s?nn\s?\}/gi, users[randomViewerThree]?.nickname || users[randomViewerThree]?.displayName || randomViewerThree)
+
+        // {numberX} - Random number from 1 to X (can also be negative)
         .replace(regexNumber, (occurrence) => Math.ceil(Math.random() * Number(occurrence.split(regexNumber)[1])))
+
+        // Context emotes
         .replace(/\{\s?lem(on)?\s?\}/gi, () => getContextEmote(`lemon`, channel))
         .replace(/\{\s?neu(tral)?\s?\}/gi, () => getContextEmote(`neutral`, channel))
-        .replace(/\{\s?hype\s?\}/gi, () => getContextEmote(`hype`, channel))
+        .replace(/\{\s?h(ype)?\s?\}/gi, () => getContextEmote(`hype`, channel))
         .replace(/\{\s?pos(itive)?\s?\}/gi, () => getContextEmote(`positive`, channel))
         .replace(/\{\s?up(set)?\s?\}/gi, () => getContextEmote(`upset`, channel))
         .replace(/\{\s?neg(ative)?\s?\}/gi, () => getContextEmote(`negative`, channel))
         .replace(/\{\s?greet(ing)?\s?\}/gi, () => getContextEmote(`greeting`, channel))
-        .replace(/\{\s?bye\s?\}/gi, () => getContextEmote(`bye`, channel))
+        .replace(/\{\s?b(ye)?\s?\}/gi, () => getContextEmote(`bye`, channel))
         .replace(/\{\s?dumb?\s?\}/gi, () => getContextEmote(`dumb`, channel))
+
+        // {n}, {noun}, {v}, {verb}, {a}, {adj}, and {adjective} - Random nouns, verbs, and adjectives
         .replace(/\{\s?n(oun)?\s?\}/gi, () => chooseFrom(nouns))
         .replace(/\{\s?v(erb)?\s?\}/gi, () => chooseFrom(verbs))
-        .replace(/\{\s?adj(ective)?\s?\}/gi, () => chooseFrom(adjectives))
+        .replace(/\{\s?(a(djective)?|adj(ective)?)\s?\}/gi, () => chooseFrom(adjectives))
+
+        // {1} through {9} - Individual words from the message
         .replace(/\{\s?1\s?\}/g, args[0] || ``)
         .replace(/\{\s?2\s?\}/g, args[1] || ``)
         .replace(/\{\s?3\s?\}/g, args[2] || ``)
@@ -64,6 +88,8 @@ function applyVariables(str, props) {
         .replace(/\{\s?7\s?\}/g, args[6] || ``)
         .replace(/\{\s?8\s?\}/g, args[7] || ``)
         .replace(/\{\s?9\s?\}/g, args[8] || ``)
+
+        // {random "one" "two" "et cetera"} - Choose a random word/phrase from a list
         .replace(regexRandom, (occurrence) => {
             const capturedArr = occurrence
                 .split(regexQuote)
