d63f9b3f7fbd1c18d54a100198b2c9fb0c56f2f1
jordbort <jordan.bortner@gmail.com>
2026-08-02 23:39:21 -0400

    Add updates

--- commands/getInfo.js
+++ commands/getInfo.js
@@ -7,6 +7,8 @@ module.exports = {
         logMessage([`> sayOnlineTime(channel: '${channel}')`])
 
         const newFeatures = [
+            `Updated !lemcmd {number#} to allow for + - * / offset`,
+            `Removed !mystats command`,
             `Updated !lemcm variables to include {streamernn} and {viewernn}`,
             `Added !followage command`,
             `Adjusted !list search functionality`,
