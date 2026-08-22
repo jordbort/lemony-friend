b367c645503e66e3a68ec518b72b58e6aa962a89
jordbort <jordan.bortner@gmail.com>
2026-08-21 23:28:18 -0400

    Implement nullish coalescing

--- utils.js
+++ utils.js
@@ -1412,8 +1412,8 @@ module.exports = {
         lemonyFresh[channel] = { ...lemonyFresh[channel] }
         lemonyFresh[channel].accessToken = lemonyFresh[channel].accessToken || mods[channel]?.accessToken || ``
         lemonyFresh[channel].refreshToken = lemonyFresh[channel].refreshToken || mods[channel]?.refreshToken || ``
-        lemonyFresh[channel].subRaidMessage = lemonyFresh[channel].subRaidMessage || ``
-        lemonyFresh[channel].noSubRaidMessage = lemonyFresh[channel].noSubRaidMessage || ``
+        lemonyFresh[channel].subRaidMessage = lemonyFresh[channel].subRaidMessage ?? ``
+        lemonyFresh[channel].noSubRaidMessage = lemonyFresh[channel].noSubRaidMessage ?? ``
         lemonyFresh[channel].followEmotes = lemonyFresh[channel].followEmotes || []
         lemonyFresh[channel].subEmotes = lemonyFresh[channel].subEmotes || []
         lemonyFresh[channel].bttvEmotes = lemonyFresh[channel].bttvEmotes || []
@@ -1431,13 +1431,13 @@ module.exports = {
         }
         lemonyFresh[channel].funnyCommands = lemonyFresh[channel].funnyCommands || []
         lemonyFresh[channel].redeems = lemonyFresh[channel].redeems || []
-        lemonyFresh[channel].rollFunNumber = lemonyFresh[channel].rollFunNumber === undefined ? true : lemonyFresh[channel].rollFunNumber
-        lemonyFresh[channel].funTimer = lemonyFresh[channel].funTimer === undefined ? 0 : lemonyFresh[channel].funTimer
-        lemonyFresh[channel].funTimerGuesser = lemonyFresh[channel].funTimerGuesser === undefined ? `` : lemonyFresh[channel].funTimerGuesser
-        lemonyFresh[channel].pollId = lemonyFresh[channel].pollId === undefined ? `` : lemonyFresh[channel].pollId
+        lemonyFresh[channel].rollFunNumber = lemonyFresh[channel].rollFunNumber ?? true
+        lemonyFresh[channel].funTimer = lemonyFresh[channel].funTimer ?? 0
+        lemonyFresh[channel].funTimerGuesser = lemonyFresh[channel].funTimerGuesser ?? ``
+        lemonyFresh[channel].pollId = lemonyFresh[channel].pollId ?? ``
         lemonyFresh[channel].viewers = []
-        lemonyFresh[channel].streakThreshold = lemonyFresh[channel].streakThreshold === undefined ? 3 : lemonyFresh[channel].streakThreshold
-        lemonyFresh[channel].streamerEmoteStreakThreshold = lemonyFresh[channel].streamerEmoteStreakThreshold === undefined ? 4 : lemonyFresh[channel].streamerEmoteStreakThreshold
+        lemonyFresh[channel].streakThreshold = lemonyFresh[channel].streakThreshold ?? 3
+        lemonyFresh[channel].streamerEmoteStreakThreshold = lemonyFresh[channel].streamerEmoteStreakThreshold ?? 4
         lemonyFresh[channel].count = {
             name: ``,
             value: 0,
